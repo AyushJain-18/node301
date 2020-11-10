f9226aae7149ceb6261fca4d6eb2697459eaf239
Ayush Jain <ayush.ayushjain12@gmail.com>
2020-11-10 21:43:40 +0530

    winston added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express       = require('express');
 const app           = express();
 const createError   = require('http-errors');
 const morgan        = require('morgan');
+const winston       = require('winston');
 
 const connectDB     = require('./db/connection');
 const restaurantRouter =  require('./routes/restaurantRouter');
@@ -12,6 +13,26 @@ connectDB();
 
 // create "middleware"
 app.use(morgan('combined')); 
+
+const logger = winston.createLogger({
+    level : 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ],
+  });
+   
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
 // Routers
 app.get('/', (req,res)=>  res.send('Hello world'))
 
